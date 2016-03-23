4a343d2cf24f003b84f9c8a5e1ab6d4b6418d872
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-23 15:00:17 -0700

    Additional solution for question 6

--- google_shopping.js
+++ google_shopping.js
@@ -7,11 +7,15 @@ var productDetails = [];
 
 var printAllItems = function() {
 	for (i = 0; i < data.items.length; i++) {
+		productDetails = [];
+		for (j = 0; j < data.items[i].product.images.length; j++){
+			productDetails.push(data.items[i].product.images[j].link);
+		}
 		products.push({
-		brand: (data.items[i].product.brand),
-		price: (data.items[i].product.inventories[0].price),
-		link: (data.items[i].product.images[0].link)});
-		// products.push(productDetails);
+			brand: (data.items[i].product.brand),
+			price: (data.items[i].product.inventories[0].price),
+			link: productDetails
+		});
 	}
 	console.log(products);
 }
