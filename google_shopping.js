605265d89ecb5aea068ec5efa7e9db19085cd154
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-23 14:40:27 -0700

    refactored question 6 with product objects

--- google_shopping.js
+++ google_shopping.js
@@ -7,11 +7,11 @@ var productDetails = [];
 
 var printAllItems = function() {
 	for (i = 0; i < data.items.length; i++) {
-		productDetails = [];
-		productDetails.brand = (data.items[i].product.brand);
-		productDetails.price = (data.items[i].product.inventories[0].price);
-		productDetails.link = (data.items[i].product.images[0].link);
-		products.push(productDetails);
+		products.push({
+		brand: (data.items[i].product.brand),
+		price: (data.items[i].product.inventories[0].price),
+		link: (data.items[i].product.images[0].link)});
+		// products.push(productDetails);
 	}
 	console.log(products);
 }
